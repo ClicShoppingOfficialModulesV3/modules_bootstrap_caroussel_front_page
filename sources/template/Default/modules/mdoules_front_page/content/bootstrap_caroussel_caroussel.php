76a144b203940ab93ca73b20cdb5850fcc8e3874
ClicShopping <loic.richard2@gmail.com>
2022-12-22 13:59:42 -0500

    update

---
+++ sources/template/Default/modules/mdoules_front_page/content/bootstrap_caroussel_caroussel.php
@@ -0,0 +1,11 @@
+<div class="col-md-<?php echo $content_width; ?>">
+  <?php echo  $indicators . $wrapper_slides; ?>
+  <a class="carousel-control-prev" href="#myCarouselHeaderBoostrapCaroussel" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#myCarouselHeaderBoostrapCaroussel" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
